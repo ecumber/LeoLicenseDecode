f8dcd7c0d7de66c7fec1c6fafe8c87427bbce8f5
ecumber <14047530+ecumber@users.noreply.github.com>
2024-07-17 21:12:37 -0700

    Add project files.

---
+++ LeoLicenseDecode/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Buffers;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Buffers.Text;
+using System.Text;
+namespace LeoLicenseDecode;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string dir = "";
+        if (args.Length == 0)
+            dir = ".";
+        else
+            dir = args[1];
+        foreach (string file in Directory.EnumerateFiles(dir, "*"))
+        {
+            FileInfo info = new FileInfo(file);
+            long size = info.Length;
+            XmlDocument license = new XmlDocument();
+
+
+
+            try
+            {
+                license.Load(file);
+            }
+            catch
+            {
+                break;
+            }
+            XmlNode root = license.DocumentElement;
+            if (root != null)
+            {
+                string license_decoded = Encoding.UTF8.GetString(Convert.FromBase64String(root.FirstChild.InnerText));
+
+
+            }
+        }
+    }
+}
